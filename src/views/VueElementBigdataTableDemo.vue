868eef0948a245db7a7f455a5f969df7609a79ac
cag2050 <cag2050@163.com>
2020-08-27 19:35:16 +0800

    add: demo

---
+++ src/views/VueElementBigdataTableDemo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <el-bigdata-table :data="tableData" height="500" :rowHeight="32">
+            <!--            <el-table-column type="selection" :selectable="selectable" width="55"></el-table-column>-->
+            <el-table-column type="selection" width="55"></el-table-column>
+            <el-table-column prop="ip" label="IP"></el-table-column>
+        </el-bigdata-table>
+    </div>
+</template>
+
+<script>
+import ElBigdataTable from "vue-elementui-bigdata-table";
+
+export default {
+    components: {
+        ElBigdataTable
+    },
+    data() {
+        return {
+            tableData: [
+                {
+                    ip: "111.sss"
+                }
+            ]
+        };
+    },
+    mounted() {
+        for (let i = 0; i < 500; i++) {
+            this.tableData.push({
+                ip: '111.222.333' + String(i)
+            })
+        }
+    },
+    methods: {}
+};
+</script>
+
+<style scoped></style>
